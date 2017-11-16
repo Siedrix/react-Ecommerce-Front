7fc9b414ddee36b058049a3374c36c0cbb5ee3e8
oscar <hello@castaneda.io>
2017-11-16 17:16:17 -0600

    Base components created

---
+++ src/Components/Products/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Products extends Component {
+    render() {
+        return(
+            <p>Hola Mundo</p>
+        )
+    }
+}
+
+export default Products
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import Products from './Components/Products'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<Products />, document.getElementById('root'))

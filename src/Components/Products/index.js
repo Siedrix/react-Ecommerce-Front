79583fda301427bd2d7bca0d8408da5ec2a87330
oscar <hello@castaneda.io>
2017-11-17 01:26:35 -0600

    Products Design Done

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -11,26 +11,71 @@ class Products extends Component {
                         <h2 className="containerProducts__body--h2">Products</h2>
                     </div>
                     <div className="containerProducts__catalog">
-                        <div className="containerProducts__catalog--box">
-                            <center><img className="box__img" src={require('../../images/img1.png')} alt="Designer-Chair" /></center>
-                            <center><h4 className="box__h4">Designer Chair</h4></center>
-                            <center><p className="box__p">$ 799</p></center>
+
+                        <div className="containerProducts__catalog--general">
+                            <div className="containerProducts__catalog--box">
+                                <center><img className="box__img" src={require('../../images/img1.png')} alt="Designer-Chair" /></center>
+                                <center><h4 className="box__h4">Designer Chair</h4></center>
+                                <center><p className="box__p">$ 799</p></center>
+                            </div>
+                            <div className="containerProducts__buttons">
+                                <button className="containerProducts__buttons--add">Add Cart</button>
+                                <div className="containerProducts__buttons--group">
+                                    <button className="containerProducts__buttons--p">-</button>
+                                    <span> 1 </span>
+                                    <button className="containerProducts__buttons--s">+</button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="containerProducts__catalog--box">
-                            <center><img className="box__img" src={require('../../images/img2.png')} alt="Designer-Chair" /></center>
-                            <center><h4 className="box__h4">Table</h4></center>
-                            <center><p className="box__p">$ 1599</p></center>
+
+                        <div className="containerProducts__catalog--general">
+                            <div className="containerProducts__catalog--box">
+                                <center><img className="box__img" src={require('../../images/img2.png')} alt="Designer-Chair" /></center>
+                                <center><h4 className="box__h4">Designer Chair</h4></center>
+                                <center><p className="box__p">$ 799</p></center>
+                            </div>
+                            <div className="containerProducts__buttons">
+                                <button className="containerProducts__buttons--add">Add Cart</button>
+                                <div className="containerProducts__buttons--group">
+                                    <button className="containerProducts__buttons--p">-</button>
+                                    <span> 1 </span>
+                                    <button className="containerProducts__buttons--s">+</button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="containerProducts__catalog--box">
-                            <center><img className="box__img" src={require('../../images/img3.png')} alt="Designer-Chair" /></center>
-                            <center><h4 className="box__h4">Chic Chair</h4></center>
-                            <center><p className="box__p">$ 699</p></center>
+
+                        <div className="containerProducts__catalog--general">
+                            <div className="containerProducts__catalog--box">
+                                <center><img className="box__img" src={require('../../images/img3.png')} alt="Designer-Chair" /></center>
+                                <center><h4 className="box__h4">Designer Chair</h4></center>
+                                <center><p className="box__p">$ 799</p></center>
+                            </div>
+                            <div className="containerProducts__buttons">
+                                <button className="containerProducts__buttons--add">Add Cart</button>
+                                <div className="containerProducts__buttons--group">
+                                    <button className="containerProducts__buttons--p">-</button>
+                                    <span> 1 </span>
+                                    <button className="containerProducts__buttons--s">+</button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="containerProducts__catalog--box">
-                            <center><img className="box__img" src={require('../../images/img4.png')} alt="Designer-Chair" /></center>
-                            <center><h4 className="box__h4">Industrial</h4></center>
-                            <center><p className="box__p">$ 299</p></center>
+
+                        <div className="containerProducts__catalog--general">
+                            <div className="containerProducts__catalog--box">
+                                <center><img className="box__img" src={require('../../images/img4.png')} alt="Designer-Chair" /></center>
+                                <center><h4 className="box__h4">Designer Chair</h4></center>
+                                <center><p className="box__p">$ 799</p></center>
+                            </div>
+                            <div className="containerProducts__buttons">
+                                <button className="containerProducts__buttons--add">Add Cart</button>
+                                <div className="containerProducts__buttons--group">
+                                    <button className="containerProducts__buttons--p">-</button>
+                                    <span> 1 </span>
+                                    <button className="containerProducts__buttons--s">+</button>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </div>

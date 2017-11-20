ffef6f6284b6fe301d0f6e7789306f2d39e410ad
oscar <hello@castaneda.io>
2017-11-20 17:52:43 -0600

    Failed interaction of components

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,22 +3,44 @@ import React, { Component } from 'react'
 import Cart from '../Cart'
 import Orders from '../Orders'
 import Products from '../Products'
-
+import SingleProduct from '../SingleProduct'
 
 
 import './app.css'
 
 class App extends Component {
 
+    constructor() {
+        super()
+        this.state = {
+            data: [{
+                display: false,
+            }]
+        }
+    }
+
+    insertDIV(event) {
+
+        this.setState({ display: true })
+    }
+
+    renderDIV() {
+        if (this.state.display) {
+            return <div className="containerCart__box"><SingleProduct /></div>
+        }
+    }
 
     render() {
-        return(
+        return (
             <div className="container">
                 <div className="container__body">
                     <Products />
                     <div className="containerDouble">
                         <Cart />
-                        <Orders />
+                        <Orders
+                            displayDIV={this.insertDIV.bind(this)}
+                        />
+                        {this.renderDIV()}
                     </div>
                 </div>
             </div>
